d047ffa156b8c9a7bab1a693dbe528be52975a11
Django <matheuscumth@gmail.com>
2023-01-02 19:50:42 -0300

    update install script and tmux/urxvt

--- install.sh
+++ install.sh
@@ -26,24 +26,24 @@ ask() {
 dir=$(pwd)
 
 if ask "Install symlink for bash" Y; then
-   ln -svf "${dir}/bash/.bashrc" "${HOME}/.bashrc"
-   ln -svf "${dir}/bash/.bash_aliases" "${HOME}/.bash_aliases"
+#   ln -svf "${dir}/bash/.bashrc" "${HOME}/.bashrc"
+#   ln -svf "${dir}/bash/.bash_aliases" "${HOME}/.bash_aliases"
   echo "yes"
 fi
 
 if ask "Install symlink for tmux" Y; then
-   ln -svf "${dir}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
+#   ln -svf "${dir}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
   echo "yes"
 fi
 
 if ask "Install symlink for urxvt?" Y; then
-  ln -svf ${dir}/urxvt/.Xresources ${HOME}/.Xresources
-  ln -svf ${dir}/urxvt/.urxvt ${HOME}/.urxvt
+#  ln -svf ${dir}/urxvt/.Xresources ${HOME}/.Xresources
+#  ln -svf ${dir}/urxvt/.urxvt ${HOME}/.urxvt
   echo "yes"
 fi
 
 if ask "Install symlink for vim" Y; then
-   ln -svf "${dir}/vim/.vimrc" "${HOME}/.vimrc"
+#   ln -svf "${dir}/vim/.vimrc" "${HOME}/.vimrc"
   echo "yes"
 fi
 
