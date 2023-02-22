ce287840d02028d92c3a07b64e899effff262518
Django <matheuscumth@gmail.com>
2023-02-22 00:02:35 -0300

    install.sh working

--- install.sh
+++ install.sh
@@ -20,30 +20,30 @@ ask() {
     case "$REPLY" in
       Y*|y*) return 0 ;;
       N*|n*) return 1 ;;
+      q*|Q*) exit 0 ;;
     esac
   done
 }
 dir=$(pwd)
 
 if ask "Install symlink for bash" Y; then
-#   ln -svf "${dir}/bash/.bashrc" "${HOME}/.bashrc"
-#   ln -svf "${dir}/bash/.bash_aliases" "${HOME}/.bash_aliases"
-  echo "yes"
+   ln -svf "${dir}/bash/.bashrc" "${HOME}/.bashrc"
+   ln -svf "${dir}/bash/.bash_aliases" "${HOME}/.bash_aliases"
 fi
 
 if ask "Install symlink for tmux" Y; then
-#   ln -svf "${dir}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
-  echo "yes"
+   ln -svf "${dir}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
 fi
 
 if ask "Install symlink for urxvt?" Y; then
-#  ln -svf ${dir}/urxvt/.Xresources ${HOME}/.Xresources
-#  ln -svf ${dir}/urxvt/.urxvt ${HOME}/.urxvt
-  echo "yes"
+  ln -svf "${dir}/urxvt/.Xresources" "${HOME}/.Xresources"
+  ln -svf "${dir}/urxvt/.Xdefault" "${HOME}/.Xdefault"
+  ln -svf "${dir}/urxvt/.urxvt" "${HOME}/.urxvt"
 fi
 
 if ask "Install symlink for vim" Y; then
-#   ln -svf "${dir}/vim/.vimrc" "${HOME}/.vimrc"
-  echo "yes"
+   ln -svf "${dir}/vim/.vimrc" "${HOME}/.vimrc"
+   echo "PlugInstall: curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 fi
 
