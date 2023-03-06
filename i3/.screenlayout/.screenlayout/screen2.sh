50cc46bc7460e15f8ee9b8f4b05a63bff5072a07
salaam <salaam@salaam.wakanda.local>
2023-03-06 14:34:53 -0300

    removed obsolete files

--- i3/.screenlayout/.screenlayout/screen2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-3 --off --output HDMI-1 --primary --mode 1920x1080 --pos 1080x528 --rotate normal --output DVI-I-1 --off --output DP-2 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate left
