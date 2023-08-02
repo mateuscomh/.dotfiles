63a0f3fa88f2b41260c94420beb9b4ace9ecea4a
MatheusM <3mhenrique@gmail.com>
2023-08-01 22:32:30 -0300

    Reorder files and script install

--- install.sh
+++ install.sh
@@ -27,12 +27,22 @@ ask() {
 dir=$(pwd)
 
 if ask "Install symlink for bash" Y; then
-   ln -svf "${dir}/bash/.bashrc" "${HOME}/.bashrc"
-   ln -svf "${dir}/bash/.bash_aliases" "${HOME}/.bash_aliases"
+ ln -svf "${dir}/bash/.bashrc" "${HOME}/.bashrc"
+ ln -svf "${dir}/bash/.bash_aliases" "${HOME}/.bash_aliases"
 fi
 
 if ask "Install symlink for tmux" Y; then
-   ln -svf "${dir}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
+  ln -svf "${dir}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
+fi
+
+if ask "Install symlink for I3WM?:" Y; then
+  ln -svf "${dir}/i3/config/i3_gaps_config" "${HOME}/.config/i3/i3_gaps_config"
+  ln -svf "${dir}/i3/config/i3_media_config" "${HOME}/.config/i3/i3_media_config"
+fi
+
+if ask "Install symlink for I3status?:" Y; then
+  ln -svf "${dir}/i3/i3status/config" "${HOME}/.config/i3status/config"
+  ln -svf "${dir}/i3/i3status/i3status_custom.sh" "${HOME}/.config/i3status/i3statusi3status_custom.sh"
 fi
 
 if ask "Install symlink for urxvt?" Y; then
@@ -42,9 +52,9 @@ if ask "Install symlink for urxvt?" Y; then
 fi
 
 if ask "Install symlink for vim" Y; then
-   ln -svf "${dir}/vim/.vimrc" "${HOME}/.vimrc"
-   mkdir "~/.vim/autoload/"
-   echo "PlugInstall: curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+  ln -svf "${dir}/vim/.vimrc" "${HOME}/.vimrc"
+  mkdir "~/.vim/autoload/"
+  echo "PlugInstall: curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 fi
 
