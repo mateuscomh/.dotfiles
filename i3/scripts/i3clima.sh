9ceddeaec6345ca7c42dcd642e3193696756c33e
mateuscomh <matheuscumth@gmail.com>
2024-06-08 13:00:15 -0300

    Update i3clima Add day of week

--- i3/scripts/i3clima.sh
+++ i3/scripts/i3clima.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
 
 get_weather_info() {
-    local result=$(curl -s wttr.in\?format=1 | sed 's/ //g')
+    local result
+    result=$(curl -s wttr.in\?format=1 | sed 's/ //g')
     if [[ $result =~ [Uu]nknown || $result != *C ]]; then
-	      sed -i 's/$/./' "$FILE"
-    else
-        echo "$result" > "$FILE"
+        result="."
     fi
+    echo "$result"
+}
+
+get_day_of_week() {
+    local day
+    day=$(date +%a)
+    echo "$day"
 }
 
 # Main function
 main() {
     FILE="/home/salaam/scripts/Output/i3clima"
 
-    # Fetch weather information and write to file
-    get_weather_info
-}
+    # Fetch weather information and day of the week
+    local weather_info
+    weather_info=$(get_weather_info)
+    local day_of_week
+    day_of_week=$(get_day_of_week)
 
-# Call the main function
+    echo "$day_of_week.$weather_info" > "$FILE"
+}
 main
+
