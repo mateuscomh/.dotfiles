a7ed682d22d0b37cfc5484c5fad3072702d89af9
mateuscomh <matheuscumth@gmail.com>
2024-08-14 13:58:12 -0300

    feature: set fade

--- i3/scripts/fade_brightness.sh
+++ i3/scripts/fade_brightness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 set -e
+
 # Função para obter a lista de saídas conectadas e ativas
 get_active_outputs() {
     xrandr --query | awk '/ connected / { 
@@ -8,38 +8,37 @@ get_active_outputs() {
             print $1 
     }'
 }
-outputs=($(get_active_outputs))
+
+# Obtenha a lista de saídas ativas
+output=($(get_active_outputs))
 
 # Brilho inicial e final
 start_brightness=1.0
-end_brightness=0.2
+end_brightness=0.3
 
 # Quantidade de passos para o fade
 steps=15
 
 # Tempo entre cada passo (em segundos)
-delay=0.08
+delay=0.05
 
+# Função para restaurar o brilho original
 restore_brightness() {
-    local active_outputs=($(get_active_outputs))
-    
-    for output in "${active_outputs[@]}"; do
-        echo "Restaurando brilho para $output"
-        xrandr --output "$output" --brightness $start_brightness
-    done
+    xrandr --output $output --brightness $start_brightness
     exit 0
 }
 
-trap restore_brightness SIGINT SIGTERM SIGHUP SIGABRT
-sleep 1
-
-for p in $(seq 0 2 100); do
+trap restore_brightness SIGINT SIGTERM SIGHUP SIGABRT SIGUSR1
+# Captura interrupções e restaura o brilho original
+current=0
+while [ "$current" -le 100 ]; do
     dunstify --icon preferences-desktop-screensaver \
-        -h int:value:"$p" \
+        -h int:value:"$current" \
         -h 'string:hlcolor:#ff4444' \
         -h string:x-dunst-stack-tag:progress-lock \
-        --timeout=500 "Bloqueio de Tela ..." $echo "$(date '+%Y-%m-%d %H:%M:%S')"
+        --timeout=500 "Bloqueio de Tela ..." "$(date '+%Y-%m-%d %H:%M:%S')"
     sleep 0.05
+    current=$((current + 2))
 done
 
 # Calcula a diferença de brilho por passo
@@ -48,24 +47,16 @@ brightness_step=$(echo "($start_brightness - $end_brightness) / $steps" | bc -l)
 # Loop para diminuir o brilho gradualmente
 current_brightness=$start_brightness
 while (( $(echo "$current_brightness > $end_brightness" | bc -l) )); do
-    echo "Ajustando brilho para $current_brightness"
-    for output in "${outputs[@]}"; do
-        echo "Ajustando brilho para $output"
-        xrandr --output "$output" --brightness "$current_brightness"
-    done
+    xrandr --output "$output" --brightness "$current_brightness"
     current_brightness=$(echo "$current_brightness - $brightness_step" | bc -l)
     sleep $delay
 done
 
 # Mantém o brilho no valor final
-echo "Mantendo brilho final $end_brightness"
-for output in "${outputs[@]}"; do
-    xrandr --output "$output" --brightness $end_brightness
-done
+xrandr --output $output --brightness $end_brightness
 
 # Aguarda um pouco antes de restaurar o brilho (opcional)
-sleep 1.5
+sleep 1.0
 
 # Restaura o brilho original
 restore_brightness
-
