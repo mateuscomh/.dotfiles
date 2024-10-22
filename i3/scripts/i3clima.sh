9493229b41701122381ec6515e7157801d551ddf
mateuscomh <matheuscumth@gmail.com>
2024-10-22 11:23:00 -0300

    feat: add alternative i3clima wheater

--- i3/scripts/i3clima.sh
+++ i3/scripts/i3clima.sh
@@ -4,7 +4,23 @@ get_weather_info() {
     local result
     result=$(curl -s wttr.in\?format=1 | sed 's/ //g')
     if [[ $result =~ [Uu]nknown || ! $result =~ [0-9]+.*C$ ]]; then
-        result="."
+        result=$(curl -s -X GET "https://api.wsclima.com.br/v1/stations/143/detail" \
+            -H "Accept: application/json" \
+            -H "Accept-Language: pt-BR,pt;q=0.7" \
+            -H "Access-Control-Allow-Origin: *" \
+            -H "Connection: keep-alive" \
+            -H "Content-Type: application/json" \
+            -H "DNT: 1" \
+            -H "Origin: https://www.wsclima.com.br" \
+            -H "Referer: https://www.wsclima.com.br/" \
+            -H "Sec-Fetch-Dest: empty" \
+            -H "Sec-Fetch-Mode: cors" \
+            -H "Sec-Fetch-Site: same-site" \
+            -H "Sec-GPC: 1" \
+            -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" \
+            -H 'sec-ch-ua: "Brave";v="129", "Not=A?Brand";v="8", "Chromium";v="129"' \
+            -H "sec-ch-ua-mobile: ?0" \
+            -H "sec-ch-ua-platform: 'Linux'" | grep -oP '"temp":"\K[0-9]+\.[0-9]+' | head -n 1)
     fi
     echo "$result"
 }
