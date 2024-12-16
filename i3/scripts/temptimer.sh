9d588cb93f9564f8219440cb83e7fedaaef8cd81
mateuscomh <matheuscumth@gmail.com>
2024-12-16 11:55:37 -0300

    fix: syntax of menssage timeout

--- i3/scripts/temptimer.sh
+++ i3/scripts/temptimer.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+USAGE="
+   ■  ▄ ▄▄▄▄  ▗▞▀▚▖ ▄▄▄ ▄▄▄ ▐▌   ▗▞▀▚▖█ █ 
+▗▄▟▙▄▖▄ █ █ █ ▐▛▀▀▘█   ▀▄▄  ▐▌   ▐▛▀▀▘█ █ 
+  ▐▌  █ █   █ ▝▚▄▄▖█   ▄▄▄▀ ▐▛▀▚▖▝▚▄▄▖█ █ 
+  ▐▌  █                     ▐▌ ▐▌     █ █ 
+  ▐▌                                      
+                                          
+                                          
+"
 # Solicita o tempo do temporizador com um valor padrão de 10 segundos
+echo -e "$USAGE"
 read -p "Defina o tempo do temporizador (ex: 10s, 5m, 1h) [10s]: " tempo
 tempo=${tempo:-10s}  # Define 10 segundos como padrão se não for especificado
 
@@ -19,7 +29,7 @@ fi
 
 # Solicita a mensagem de notificação
 read -p "Digite a mensagem para exibir ao final do temporizador: " mensagem
-mensagem=${mensagem:-"Tempo Encerrado!"}
+[[ -z "$mensagem" ]] && mensagem="em execução"
 
 echo "Iniciando temporizador $mensagem por $tempo..."
 
@@ -34,16 +44,20 @@ current=0
 while [ "$current" -le "$total_passos" ]; do
     progresso=$((current * 100 / total_passos))
     tempo_restante=$((segundos_total - current * intervalo))
+    minutos=$((tempo_restante / 60))
+    segundos=$((tempo_restante % 60))
+    
+    temporizador="Faltam $minutos minuto(s) e $segundos segundo(s)"
     dunstify --icon preferences-desktop-screensaver \
         -h int:value:"$progresso" \
         -h 'string:hlcolor:#ff4444' \
         -h string:x-dunst-stack-tag:temporizador \
-        --timeout=1010 "Temporizador em execução..." "Faltam $tempo_restante segundos"
+        --timeout=1010 "Temporizador $mensagem..." "Faltam $temporizador segundos"
     sleep "$intervalo"
     current=$((current + 1))
 done
 
 # Notificação e som final
-dunstify -u critical "Temporizador" "$mensagem $(date '+%Y-%m-%d %H:%M:%S')"
+dunstify -u critical "Temporizador" "$mensagem finalizado às: $(date '+%Y-%m-%d %H:%M:%S')"
 paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
 
